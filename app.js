7cac55e3fa6737ee2dd9f0d04edf208688a6670c
Ghulam qadir <154546196+ghulamqadir10@users.noreply.github.com>
2024-04-21 23:51:16 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,192 @@
+// var counter = localStorage.getItem("counter");
+// if (counter === null) {
+//     counter = 0;
+// } else {
+//     counter = parseInt(counter);
+// }
+// counter++
+// localStorage.setItem("counter", counter);
+// alert("You have visited (" + counter + ") times this page")
+
+
+
+// let num=7
+// for(var i=1;i<=10;i++){
+//  document.write("<h4>",num," * " ,i," = ",num*i,"</h4>")
+// }
+
+
+// let sub_1 = prompt("please enter a subject 1");
+// let sub_2 = prompt("please enter a subject 2");
+// let sub_3 = prompt("please enter a subject 3");
+// let obt_marks_sub_1 = +prompt("please enter obtained marks of subject 1");
+// let obt_marks_sub_2 = +prompt("please enter a obtained marks of subject 2 ");
+// let obt_marks_sub_3 = +prompt("please enter  obtained marks of subject 3");
+
+// let total_marks = 100;
+
+
+
+// let percent1 = (obt_marks_sub_1 / total_marks) * 100;
+// let percent2 = (obt_marks_sub_2 / total_marks) * 100;
+// let percent3 = (obt_marks_sub_3 / total_marks) * 100;
+// document.getElementById("sub1").innerHTML = sub_1;
+// document.getElementById("sub2").innerHTML = sub_2;
+// document.getElementById("sub3").innerHTML = sub_3;
+// document.getElementById("totalmarks1").innerHTML = total_marks;
+// // document.getElementById("totalmarks2").innerHTML = total_marks;
+// // document.getElementById("").innerHTML = total_marks;
+// document.getElementById("obmarks1").innerHTML = obt_marks_sub_1;
+// document.getElementById("obmarks2").innerHTML = obt_marks_sub_2;
+// document.getElementById("obmarks3").innerHTML = obt_marks_sub_3;
+// document.getElementById("percentage1").innerHTML = percent1 + "%";
+// document.getElementById("percentage2").innerHTML = percent2 + "%";
+// document.getElementById("percentage3").innerHTML = percent3 + "%";
+
+// let number =prompt("enter number ");
+// let other= number%3
+// if (other===0){
+// console.log("it is divisible by 3")
+// }else{
+//     console.log("it is not divisible by 3")
+// }
+
+// let even_odd = prompt("input a number");
+// if (even_odd % 2 === 0) {
+//     console.log(even_odd, " is even num")
+// }
+// if (even_odd % 2 !== 0) {
+//     console.log(even_odd," is an odd number");
+// }
+
+
+
+// let input = prompt("salam please input an hobby name i will let you know it is avalible or not!");
+// let users = [
+//     {
+//         name: "umair",
+//         age: 20,
+//         hobbies: ["football", "chess", "volleyball"]
+//     },
+//     {
+//         name: "samad",
+//         age: 20,
+//         hobbies: ["football", "cricket", "volleyball"]
+//     },
+//     {
+//         name: "ahmed",
+//         age: 20,
+//         hobbies: ["football", "volleyball", "volleyball"]
+//     },
+//     {
+//         name: "ali",
+//         age: 20,
+//         hobbies: ["football", "ludostar", "volleyball", "candycrush", "gta4"]
+//     }
+// ]
+// var flag=false;
+// for (var i = 0; i < users.length; i++) {
+//     for (var j = 0; j < users[i].hobbies.length; j++) {
+//         if(input ===users[i].hobbies[j]){
+//             console.log(users[i].hobbies[j]," is found")
+//             flag=true;
+//         }
+//     }
+//     // console.log(users[i].hobbies)
+// }
+// if(!flag){
+//     console.log ("user not found")
+// }
+
+
+// let input = prompt("please enter a hobby name");
+// var users = [
+//     {
+//         name: "ali",
+//         age: 21,
+//         hobbies: ["minimilitia", "ludostar", " football"]
+//     },
+//     {
+//         name: "ali",
+//         age: 21,
+//         hobbies: ["cargame", "chess", " football"]
+//     },
+//     {
+//         name: "ali",
+//         age: 21,
+//         hobbies: ["candycrush", "bedminton", " ringbell"]
+//     },
+//     {
+//         name: "ali",
+//         age: 21,
+//         hobbies: ["cricket", "volleyball", " football"]
+//     }
+// ];
+// // var flag = false;
+// // for (var i = 0; i < users.length; i++) {
+// //     for (var j = 0; j < users[i].hobbies.length; j++) {
+// //         if (input === users[i].hobbies[j]) {
+// //             console.log(users[i].hobbies[j] ,  " is found")
+
+// //             flag = true;
+// //         }
+// //     }
+// //     // break;  
+
+// // }
+// // if(!flag){
+// //    console.log("user not found")
+// // }
+
+// var flag = false;
+// for (var i = 0; i < users.length; i++) {
+//     for (var j = 0; j < users[i].hobbies.length; j++) {
+//         if (input === users[i].hobbies[j]) {
+//             console.log(users[i].hobbies[j], " is found");
+//             flag = true;
+//         }
+//     }
+// }
+// if(!flag){
+//     console.log(" user is not found");
+// }
+
+
+// new questions      
+
+
+// let input=prompt("pleaswe input a phone model i will give you a length and and display of input model");
+// console.log("phone model is " , input);
+// inputlength=input.length;
+// console.log("length of model: " inputlength);
+
+
+
+// rndom questions
+
+// let string="pakistani";
+// let letter="n";
+// var index=string.indexOf("n");
+// console.log(index)
+
+
+/// another random question
+// let string = "pakistani";
+// let letter = "n";
+// let flag=false;
+// // var split=string.split("");
+// for(i=0;i<string.length;i++){
+// if(string[i] === letter){
+//  console.log(string[i] , " is found")
+// flag=true;
+// }}
+// if(!flag){
+//     console.log("it,s not found")
+// };
+
+
+
+// //////////////////////////////////////////////xxxxxx\\\\\\\\\\\\\\\\\\\\\\\
+
+
+
